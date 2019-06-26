454409af4852d9b0281a916629ea5a6f1432f956
JasonLee <lishuo@zhihu.com>
2019-06-26 14:01:35 +0800

    release 1.0.0

--- Example/JSLDrawingBoard/ViewController.swift
+++ Example/JSLDrawingBoard/ViewController.swift
@@ -7,9 +7,90 @@
 //
 
 import UIKit
+import JSLDrawingBoard
 
 class ViewController: UIViewController {
 
-    
+    private let board = DrawingBoard()
+    private lazy var backButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle("Back", for: .normal)
+        button.addTarget(self, action: #selector(backAction), for: .touchUpInside)
+        return button
+    }()
+
+    private lazy var clearButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle("Clear", for: .normal)
+        button.addTarget(self, action: #selector(clearAction), for: .touchUpInside)
+        return button
+    }()
+
+    private lazy var colorButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle("Color", for: .normal)
+        button.addTarget(self, action: #selector(changeColor), for: .touchUpInside)
+        return button
+    }()
+
+    private lazy var strokeWidth: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle("Width", for: .normal)
+        button.addTarget(self, action: #selector(changeStrokeWidth), for: .touchUpInside)
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(board)
+        buttons().forEach { button in
+            view.addSubview(button)
+        }
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let bounds = view.bounds
+        board.frame = bounds
+        let buttonH: CGFloat = 40
+        let buttonW: CGFloat = bounds.width / CGFloat(buttons().count)
+        buttons().enumerated().forEach { i, button in
+            button.backgroundColor = randomColor()
+            button.frame = CGRect(x: buttonW * CGFloat(i), y: 50, width: buttonW, height: buttonH)
+        }
+    }
+
+    @objc
+    func backAction() {
+        board.revokeLatestStroke()
+    }
+
+    @objc
+    func clearAction() {
+        board.clearAllStrokes()
+    }
+
+    @objc
+    func changeColor() {
+        board.brushColor = randomColor()
+    }
+
+    @objc
+    func changeStrokeWidth() {
+        board.brushWidth = .random(in: 1...10)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func buttons() -> [UIButton] {
+        return [backButton, clearButton, colorButton, strokeWidth]
+    }
+
+    func randomColor() -> UIColor {
+        return UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1)
+    }
 }
 
---
+++ JSLDrawingBoard/Classes/DrawingBoard.swift
@@ -0,0 +1,153 @@
+//
+//  DrawingBoard.swift
+//  DrawingBoard
+//
+//  Created by JasonLee on 2019/6/23.
+//
+
+import UIKit
+
+@objc(JSLDrawingBoard)
+public class DrawingBoard: UIView {
+
+    //----------------------------------------------------------
+    // MARK: Properties
+
+    /// 笔划信息
+    private var strokesInfos = [StrokeInfo]()
+    /// 画笔颜色
+    @objc public var brushColor = UIColor.red
+    /// 画笔宽度
+    @objc public var brushWidth: CGFloat = 3
+
+    //----------------------------------------------------------
+    // MARK: Life Cycle
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.clear
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    //----------------------------------------------------------
+
+    // MARK: 渲染视图
+    override public func draw(_ rect: CGRect) {
+        if strokesInfos.count <= 0 {
+            return
+        }
+
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
+        context.setLineCap(.round)
+        context.setLineJoin(.round)
+
+        for info in strokesInfos {
+
+            guard let startPoint = info.points.first?.cgPointValue else {
+                return
+            }
+
+            context.beginPath()
+            context.move(to: startPoint)
+            if info.points.count > 1 {
+
+                for (indx, value) in info.points.enumerated() {
+                    if indx == 0 {
+                        continue
+                    }
+                    let point = value.cgPointValue
+                    context.addLine(to: point)
+                }
+            } else {
+                context.addLine(to: startPoint)
+            }
+
+            context.setStrokeColor(info.color.cgColor)
+            context.setLineWidth(info.width)
+            context.strokePath()
+        }
+    }
+
+    // MARK: Touch Events
+
+    override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        drawingBegin(point: touch.location(in: self))
+    }
+
+    override public func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        drawingMove(point: touch.location(in: self))
+    }
+
+    // MARK: DrawingEvent
+
+    /// 开始绘画
+    private func drawingBegin(point: CGPoint) {
+
+        let lineInfo = StrokeInfo()
+        lineInfo.width = brushWidth
+        lineInfo.color = brushColor
+        lineInfo.points.append(NSValue(cgPoint: point))
+
+        strokesInfos.append(lineInfo)
+        self.setNeedsDisplay()
+    }
+
+    /// 画笔移动
+    private func drawingMove(point: CGPoint) {
+        guard let lineInfo = strokesInfos.last else {
+            return
+        }
+        lineInfo.points.append(NSValue(cgPoint: point))
+        self.setNeedsDisplay()
+    }
+
+    @objc
+    public func hasEdited() -> Bool {
+        return strokesInfos.count > 0
+    }
+
+    /// 撤销上一步
+    @objc
+    public func revokeLatestStroke() {
+        if strokesInfos.count == 0 {
+            return
+        }
+        strokesInfos.removeLast()
+        self.setNeedsDisplay()
+    }
+
+    /// 清除所有笔划
+    @objc
+    public func clearAllStrokes() {
+        if strokesInfos.count == 0 {
+            return
+        }
+        strokesInfos.removeAll()
+        self.setNeedsDisplay()
+    }
+
+}
+
+/// 笔划信息
+fileprivate class StrokeInfo: NSObject {
+
+    /// 线条所包含的点
+    var points = [NSValue]()
+    /// 线条颜色
+    var width: CGFloat = 3
+    /// 线条颜色
+    var color = UIColor.red
+
+}
+
